3dfb5d306667abb8c5d37625b58075f8f3510b0c
Sergei <adyshkinss@gmail.com>
2021-05-24 00:44:26 +0300

    Добавьте файлы проекта.

---
+++ TrainingInformationSystem/Pages/TestPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainingInformationSystem.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TestPage.xaml
+    /// </summary>
+    public partial class TestPage : Page
+    {
+        List<string> listAnswer = new List<string>() { "1", "2", "3", "4" };
+        public TestPage()
+        {
+            InitializeComponent();
+            cmbAnswer.ItemsSource = listAnswer;
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
